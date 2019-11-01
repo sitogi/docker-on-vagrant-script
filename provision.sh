e9889c8aa98d8bf2e7fd2f84acb4fc4ff73eed36
sitogi <takano162@gmail.com>
2019-11-01 23:21:33 +0900

    [update] tmux 2.8 and Vim 8

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install Commands
-yum -y install unzip git vim tmux
+yum -y install unzip git
 
 # Disable SELinux
 setenforce 0
@@ -23,4 +23,12 @@ systemctl restart docker
 # workaround for https://github.com/tmatilai/vagrant-proxyconf/issues/206
 mkdir -p /etc/systemd/system/docker.service.d
 touch /etc/systemd/system/docker.service.d/http-proxy.conf
+
+# install tmux 2.8 in CentOS 7
+curl https://gist.githubusercontent.com/pokev25/4b9516d32f4021d945a140df09bf1fde/raw/d7b391ab715890ab8312c8b2c59798e3b0852645/install-tmux.sh > install-tmux.sh
+chmod +x install-tmux.sh
+
+# install vim 8
+curl -L https://copr.fedorainfracloud.org/coprs/unixcommunity/vim/repo/epel-7/unixcommunity-vim-epel-7.repo -o /etc/yum.repos.d/unixcommunity-vim-epel-7.rep://copr.fedorainfracloud.org/coprs/unixcommunity/vim/repo/epel-7/unixcommunity-vim-epel-7.repo -o /etc/yum.repos.d/unixcommunity-vim-epel-7.repo
+yum install vim
 
